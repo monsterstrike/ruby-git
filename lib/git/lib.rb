3c4d537e61d395734b5610570bb36fa0895241c3
hiroaki.murayama <hotsoup.h@gmail.com>
2016-02-22 15:01:40 +0900

    Add --full-index option on git diff.

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -345,6 +345,7 @@ module Git
     def diff_full(obj1 = 'HEAD', obj2 = nil, opts = {})
       diff_opts = ['-p']
       diff_opts << obj1
+      diff_opts << '--full-index'
       diff_opts << obj2 if obj2.is_a?(String)
       diff_opts << '--' << opts[:path_limiter] if opts[:path_limiter].is_a? String
 
